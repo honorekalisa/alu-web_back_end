6a657f5456e476bc1be7832dd82cd1a81229809d
honorekalisa <h.kalisa@alustudent.com>
2024-10-31 15:29:53 +0200

    ES6_promise

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,5 +1,5 @@
 export default function getNeughborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
